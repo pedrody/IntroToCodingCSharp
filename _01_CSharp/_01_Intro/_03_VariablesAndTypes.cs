8967ea534be4bdecd315c6ab108183ba940fb554
pedrody <pedroazevedo-680@hotmail.com>
2025-11-12 22:58:30 -0300

    Aula 2: Introdução, Hello World, Comments and Variáveis

--- _01_CSharp/_01_Intro/_03_VariablesAndTypes.cs
+++ _01_CSharp/_01_Intro/_03_VariablesAndTypes.cs
@@ -40,19 +40,60 @@ class VariablesAndTypes
 {
   public static void Main(string[] args)
   {
-    int age = 50;
-    Console.WriteLine(age);
+        int age = 50;
+        // System.Int32 age = 50;
+        Console.WriteLine(age);
+        Console.WriteLine($"int.MinValue = {int.MinValue}");
+        Console.WriteLine($"int.MaxValue = {int.MaxValue}");
 
-    bool approved = true;
-    Console.WriteLine(approved);
+        bool approved = true;
+        // approved = false;
+        Console.WriteLine(approved ? "You was approved" : "You was not approved");
+        Console.WriteLine(approved);
 
-    double height = 1.95;
-    Console.WriteLine(height);
+        double height = 1.95;
+        Console.WriteLine(height);
 
-    char c = 'Z';
-    Console.WriteLine(c);
+        char c = 'a';
+        Console.WriteLine(c);
 
-    string name = "Jose Maria";
-    Console.WriteLine(name);
-  }
+        string name = "Jose Azevedo";
+        Console.WriteLine(name);
+
+        string jsonString = """
+            {
+              "Date": "2019-08-01T00:00:00-07:00",
+              "TemperatureCelsius": 25,
+              "Summary": "Hot",
+              "DatesAvailable": [
+                "2019-08-01T00:00:00-07:00",
+                "2019-08-02T00:00:00-07:00"
+              ],
+              "TemperatureRanges": {
+                "Cold": {
+                  "High": 20,
+                  "Low": -10
+                },
+                "Hot": {
+                  "High": 60,
+                  "Low": 20
+                }
+                        },
+              "SummaryWords": [
+                "Cool",
+                "Windy",
+                "Humid"
+              ]
+            }
+            """;
+        Console.WriteLine(jsonString);
+
+        Person p = new Person();
+        p.Name = "Jose Azevedo (from Person)";
+        Console.WriteLine(p.Name);
+    }
+}
+
+class Person {
+    public string Name;
 }
